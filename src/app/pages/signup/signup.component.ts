80541648a47d2c5c273543bcd39d01e1556b8316
b.laura <b.laura0269@gmail.com>
2023-05-07 19:47:27 +0200

    Major changes

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,56 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Location} from "@angular/common";
+import {AuthService} from "../../shared/services/auth.service";
+import {User} from "../../shared/models/User";
+import {UserService} from "../../shared/services/user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit{
+  signUpForm=new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rePassword:new FormControl(''),
+    name: new FormGroup({
+      firstname:new FormControl(''),
+      lastname:new FormControl('')
+    })
+  })
+
+constructor(private location: Location, private authService: AuthService, private userService: UserService, private router: Router) {
+}
+ngOnInit() {
+
+}
+onSubmit(){
+    console.log(this.signUpForm.value);
+    this.authService.signup(this.signUpForm.get('email')?.value as string, this.signUpForm.get('password')?.value as string).then(cred =>{
+      console.log(cred);
+      const user: User={
+        id:cred.user?.uid as string,
+        email: this.signUpForm.get('email')?.value as string,
+        username: this.signUpForm.get('email')?.value?.split('@')[0] as string,
+        name:{
+          firstname: this.signUpForm.get('name.firstname')?.value as any,
+          lastname: this.signUpForm.get('name.lastname')?.value as any
+        }
+      };
+      this.userService.create(user).then(_ =>{
+        console.log('User added successfully! :D');
+        this.router.navigateByUrl('/welcome');
+      }).catch(error=>{
+        console.error(error);
+      });
+    }).catch(error=>{
+      console.error(error);
+    });
+}
+goBack(){
+    this.location.back();
+}
+}
